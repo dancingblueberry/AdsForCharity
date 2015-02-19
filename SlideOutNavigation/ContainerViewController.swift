03cc1af10de35e29d1f9444f4a11b367274c2a56
Amanda Berryhill <Leia@Amandas-MacBook-Pro.local>
2015-02-18 20:45:10 -0800

    left cats slide bar

--- SlideOutNavigation/ContainerViewController.swift
+++ SlideOutNavigation/ContainerViewController.swift
@@ -9,31 +9,90 @@
 import UIKit
 import QuartzCore
 
-class ContainerViewController: UIViewController {
+enum SlideOutState {
+    case BothCollapsed
+    case LeftPanelExpanded
+    case RightPanelExpanded
+}
+
+class ContainerViewController: UIViewController, CenterViewControllerDelegate {
+    
+    var centerNavigationController: UINavigationController!
+    var centerViewController: CenterViewController!
+    var currentState: SlideOutState = .BothCollapsed
+    var leftViewController: SidePanelViewController?
+    let centerPanelExpandedOffset: CGFloat = 60
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        centerViewController = UIStoryboard.centerViewController()
+        centerViewController.delegate = self
+        
+        centerNavigationController = UINavigationController(rootViewController: centerViewController)
+        view.addSubview(centerNavigationController.view)
+        addChildViewController(centerNavigationController)
+        
+        centerNavigationController.didMoveToParentViewController(self)
     }
   
     // MARK: CenterViewController delegate methods
   
     func toggleLeftPanel() {
+        let notAlreadyExpanded = (currentState != .LeftPanelExpanded)
+        
+        if notAlreadyExpanded {
+            addLeftPanelViewController()
+        }
+        
+        animateLeftPanel(shouldExpand: notAlreadyExpanded)
     }
   
     func toggleRightPanel() {
     }
   
     func addLeftPanelViewController() {
+        if (leftViewController == nil) {
+            leftViewController = UIStoryboard.leftViewController()
+            leftViewController!.animals = Animal.allCats()
+            
+            addChildSidePanelController(leftViewController!)
+        }
+    }
+    
+    func addChildSidePanelController(sidePanelController: SidePanelViewController) {
+        view.insertSubview(sidePanelController.view, atIndex: 0)
+        
+        addChildViewController(sidePanelController)
+        sidePanelController.didMoveToParentViewController(self)
     }
     
     func addRightPanelViewController() {
     }
   
     func animateLeftPanel(#shouldExpand: Bool) {
+        if (shouldExpand) {
+            currentState = .LeftPanelExpanded
+            
+            animateCenterPanelXPosition(targetPosition: CGRectGetWidth(centerNavigationController.view.frame) - centerPanelExpandedOffset)
+        } else {
+            animateCenterPanelXPosition(targetPosition: 0) { finished in
+                self.currentState = .BothCollapsed
+                
+                self.leftViewController!.view.removeFromSuperview()
+                self.leftViewController = nil
+            }
+        }
+    }
+    
+    func animateCenterPanelXPosition(#targetPosition: CGFloat, completion: ((Bool) -> Void)! = nil) {
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .CurveEaseInOut, animations: {
+                self.centerNavigationController.view.frame.origin.x = targetPosition
+        }, completion: completion)
     }
     
     func animateRightPanel(#shouldExpand: Bool) {
     }
+    
   
     // MARK: Gesture recognizer
   
